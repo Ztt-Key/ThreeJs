242affc89c89e762b2eca2a1372a34502c5fe39e
周天泽 <1847860947@qq.com>
2025-03-08 21:06:58 +0800

    fect:完善基本threejs

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,30 @@
 <template>
-  <div class="container">登录</div>
+  <div id="container"></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup >
+import { onMounted } from 'vue';
+import * as THREE from 'three';
+//容器
+var container; 
+onMounted(() => {
+     container=document.getElementById('container')
+     initScene()
+})
+//初始化场景
+const initScene = ()=>{
+    const scene = new THREE.Scene();
+    const fog = new THREE.Fog(0x000000, 1, 10000);
+}
+//添加背景
+const initSceneBg = ()=>{
+    //纹理
+    new THREE.TextureLoader().load();
+}
+</script>
 
 <style scoped lang="less">
-.container {
+#container {
     width: 100%;
     height: 100vh;
 }
